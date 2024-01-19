bf260b7015e65fa905cc4efd99544f00de6477a1
Raziel619 <raziel619dev@gmail.com>
2024-01-18 23:50:07 -0400

    Published to pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from pathlib import Path
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-# read the contents of README file
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name="upscale_x4",
-    version="0.0.1",
-    license="MIT",
-    author="Raziel619",
-    author_email="raziel619dev@gmail.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    url="https://github.com/Raziel619/upscale_x4",
-    keywords="upscale",
-    install_requires=[],
-)
+setup()
